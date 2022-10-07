edee74352785e3b73ede2f9de94d2d82b7c406dc
noelnmar <gabriel.nmar@stud.uni-corvinus.hu>
2022-10-07 10:46:45 +0200

    Added Excel

--- Exeltask/Exeltask/Form1.cs
+++ Exeltask/Exeltask/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Exeltask
 {
+    
     public partial class Form1 : Form
     {
+
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
         
@@ -24,5 +28,7 @@ namespace Exeltask
         {
             Flats = context.Flat.ToList();
         }
+
+
     }
 }
