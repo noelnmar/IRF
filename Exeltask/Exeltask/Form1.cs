95735e4628c49e89c455014ae3d54d5a8d601ae1
noelnmar <gabriel.nmar@stud.uni-corvinus.hu>
2022-10-07 10:59:41 +0200

    Excel workbook added

--- Exeltask/Exeltask/Form1.cs
+++ Exeltask/Exeltask/Form1.cs
@@ -15,20 +15,55 @@ namespace Exeltask
     
     public partial class Form1 : Form
     {
-
         List<Flat> Flats;
         RealEstateEntities context = new RealEstateEntities();
-        
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         private void LoadData()
         {
             Flats = context.Flat.ToList();
         }
 
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
 
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
     }
 }
