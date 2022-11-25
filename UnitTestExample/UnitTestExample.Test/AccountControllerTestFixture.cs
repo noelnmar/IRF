54365d475c091fe18712dae6c3a7bc1cc62c8c6e
noelnmar <gabriel.nmar@stud.uni-corvinus.hu>
2022-11-25 10:57:56 +0100

    Password validation test

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -15,7 +15,7 @@ namespace UnitTestExample.Test
          TestCase("irf@uni-corvinus", false),
          TestCase("irf.uni-corvinus.hu", false),
          TestCase("irf@uni-corvinus.hu", true)
-         ]
+        ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -23,6 +23,25 @@ namespace UnitTestExample.Test
 
             // Act
             var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+         TestCase("ABCD1234", false),
+         TestCase("Ab1234", false),
+         TestCase("Abcd1234", true),
+         TestCase("abcd1234", false),
+         TestCase("abcdABCD", false)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
