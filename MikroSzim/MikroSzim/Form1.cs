4bd45a5b8abcaf7b0fe7d9dce2a5e5f721f8435a
noelnmar <gabriel.nmar@stud.uni-corvinus.hu>
2022-12-02 10:12:40 +0100

    Generating random number

--- MikroSzim/MikroSzim/Form1.cs
+++ MikroSzim/MikroSzim/Form1.cs
@@ -19,7 +19,8 @@ namespace MikroSzim
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
-        
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
